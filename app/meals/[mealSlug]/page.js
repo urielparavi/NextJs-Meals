89c2cf6f36b263f2cff547d0f3a11ba947bb95b4
Your Name <urielparavi75@gmail.com>
2025-05-27 21:12:35 +0300

    114. Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,45 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  // Replace all newline characters (\n) with <br/> tags
+  // \n        = newline character
+  // /\n/g     = regular expression to match ALL newline characters (g = global)
+  // <br/>     = HTML line break tag to preserve formatting in the browser
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        {/* 
+      ⚠️ Using dangerouslySetInnerHTML:
+      We're injecting raw HTML directly into the DOM.
+      This is meant to be used when you receive a ready-made HTML template (like from a CMS, rich text editor, or Markdown converted to HTML) from a trusted source.
+      Make sure the HTML comes from a trusted source — otherwise it can lead to XSS vulnerabilities.
+      If the content comes from users or external sources, sanitize it first using a library like DOMPurify.
+*/}
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
