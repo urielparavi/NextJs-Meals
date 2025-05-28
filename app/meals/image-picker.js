abbb2e99959779bda91a96f56d69a053da08265a
Your Name <urielparavi75@gmail.com>
2025-05-28 21:57:40 +0300

    120. Introducing & Using Server Actions for Handling Form Submissions

--- app/meals/image-picker.js
+++ app/meals/image-picker.js
@@ -93,6 +93,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
           // multiple <-- add this attribute to allow selecting multiple files
         />
         <button
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,39 @@ import ImagePicker from '../image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  // ✅ This is a Server Action:
+  // A server-side function that's triggered by a client-side event,
+  // like a form submission or button click.
+  //
+  // 🔄 How it works:
+  // - The user submits a form (in the browser)
+  // - The browser sends the form data to the server using `action={serverAction}`
+  // - Next.js executes this function on the server (because of 'use server')
+  //
+  // 🛠️ In this function you can:
+  // - Save data to a database
+  // - Upload files
+  // - Send emails
+  // - Return responses (like redirects)
+  //
+  // 💡 Why it's great:
+  // - No need for `onSubmit`
+  // - No need for `fetch` or API routes
+  // - It's secure, simple, and fully server-powered
+  async function shareMeal(formData) {
+    'use server';
+
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email'),
+    };
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +44,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +72,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
