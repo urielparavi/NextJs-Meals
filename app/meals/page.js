d66812ca1943bc2362e8d974053c5d8d617fb343
Your Name <urielparavi75@gmail.com>
2025-05-31 09:59:03 +0300

    128. Building For Production & Understanding NextJS Caching

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,8 @@ import { getMeals } from '@/lib/meals';
 import { Suspense } from 'react';
 
 async function Meals() {
+  console.log('Fetching meals');
+
   const meals = await getMeals();
 
   return <MealsGrid meals={meals} />;
